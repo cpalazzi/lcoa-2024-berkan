922e2176ea2d1a6aa6b9d5ca3fcc605b0a80c9fc
nsalmon11 <80899918+nsalmon11@users.noreply.github.com>
2023-05-03 10:56:49 +0100

    New version can draw updated costs more easily from CSVs rather than always referring to a plant design file.
    Updated multi_site optionality.
    
    Adding section on plant operation rather than just plant design
    
    Added in class for handling large datasets.

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import p_auxiliary as aux
 import glob
 import p_data_store as pds
 import pandas as pd
+import p_location_class as plc
+import geopandas as gpd
+from shapely.geometry import Point
 
 """File to optimise the size of a green ammonia plant given a specified wind and solar profile"""
 
 
-def generate_network(n_snapshots, data_file):
+def generate_network(n_snapshots, data_file, aggregation_count=1, costs=None, efficiencies=None):
     """Generates a network that can be used to run several cases"""
     # ==================================================================================================================
     # Set up network
@@ -17,22 +20,32 @@ def generate_network(n_snapshots, data_file):
     network = pypsa.Network(override_component_attrs=aux.create_override_components())
 
     # Set the time values for the network
-    network.set_snapshots(range(n_snapshots))
+    network.set_snapshots(range(int(n_snapshots)))
 
     # Import the design of the H2 plant into the network
     network.import_from_csv_folder(data_file)
 
+    if costs is not None:
+        for equipment, row in costs.items():
+            for df in [network.links, network.generators, network.stores]:
+                if equipment in df.index:
+                    df.loc[equipment, 'capital_cost'] = row
+
+    if efficiencies is not None:
+        for equipment, row in efficiencies.items():
+            if equipment in network.links.index:
+                network.links.loc[equipment, 'efficiency'] = row
+
+    network.stores.capital_cost *= aggregation_count
+
     return network
 
 
-def main(n=None, file_name=None, weather_data=None, multi_site=False, extension=None, aggregation_count=1):
+def main(n=None, file_name=None, weather_data=None, multi_site=False, extension="", aggregation_count=1):
     """Code to execute run at a single location"""
     # Import the weather data
     if file_name is not None and weather_data is None:
         weather_data = aux.get_weather_data(file_name=file_name, aggregation_count=aggregation_count)
-    elif file_name is not None and weather_data is not None:
-        raise RuntimeWarning('You have entered both a file_name and weather_data; '
-                             'the weather_data is being used, and any data in the file will be ignored.')
 
     # Import a generic network if needed
     if n is None:
@@ -64,17 +77,21 @@ def main(n=None, file_name=None, weather_data=None, multi_site=False, extension=
     # Check if the CAPEX input format in Basic_H2_plant is correct, and fix it up if not
     # ==================================================================================================================
 
-    CAPEX_check = aux.check_CAPEX(file_name=file_name)
-    if CAPEX_check is not None:
-        for item in [n.generators, n.links, n.stores]:
-            item.capital_cost = item.capital_cost * CAPEX_check[1]/100 + item.capital_cost/CAPEX_check[0]
+    if not multi_site:
+        CAPEX_check = aux.check_CAPEX(file_name=file_name)
+        if CAPEX_check is not None:
+            for item in [n.generators, n.links, n.stores]:
+                item.capital_cost = item.capital_cost * CAPEX_check[1]/100 + item.capital_cost/CAPEX_check[0]
 
     # ==================================================================================================================
     # Solve the model
     # ==================================================================================================================
 
-    # Ask the user how they would like to solve
-    solver, _ = aux.get_solving_info(file_name=file_name)
+    # Ask the user how they would like to solve, unless they're doing several cases
+    if not multi_site:
+        solver, _ = aux.get_solving_info(file_name=file_name)
+    else:
+        solver = 'gurobi'
 
     # Implement their answer
     n.lopf(solver_name=solver, pyomo=True, extra_functionality=aux.pyomo_constraints)
@@ -91,17 +108,79 @@ def main(n=None, file_name=None, weather_data=None, multi_site=False, extension=
         output = aux.get_results_dict_for_excel(n, scale, aggregation_count=aggregation_count)
 
         # Send the results to excel
-        aux.write_results_to_excel(output, file_name=file_name[12:], extension=extension)
+        aux.write_results_to_excel(output, file_name=file_name[5:], extension=extension)
     else:
         output = aux.get_results_dict_for_multi_site(n, aggregation_count=aggregation_count)
 
     return output
 
 
-if __name__ == '__main__':
-    file_lst = glob.glob('WeatherData/*.csv')
-    length = len(pd.read_csv(file_lst[0]))
-    network_design = generate_network(length, 'Basic_ammonia_plant_2030')
+def run_Alli_sites(year):
+    # Download any necessary data
+    excel_file = r'Data\UkraineCostData.xlsx'
+    costs = pd.read_excel(excel_file, sheet_name='Costs').set_index('Equipment')[year]
+    efficiencies = pd.read_excel(excel_file, sheet_name='Efficiencies').set_index('Equipment')[year]
+
+    # Create a network; override the defaults with the relevant cost and efficiency data
+    n = generate_network(8760, 'Basic_ammonia_plant_2030', costs=costs, efficiencies=efficiencies)
+
+    # Get a list of the renewables
+    renewables = n.generators.index.to_list()
+
+    # Create a place to store the data between runs
     store = pds.Data_store()
-    for file in glob.glob('Data/*'):
-        main(n=network_design, file_name=file, multi_site=True, extension='_2030', aggregation_count=8)
+
+    # Get the weather data
+    data = plc.all_locations(r'C:\Users\worc5561\OneDrive - Nexus365\Coding\Offshore_Wind_model')
+
+    # Get a map of the world to identify which country the weather data is in
+    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres')).rename(columns={'name': 'country'})
+
+    # List hte countries you're interested in
+    country_lst = ['Ukraine']
+    # country_lst = pd.read_csv(r'C:\Users\worc5561\OneDrive - Nexus365\Coding\Offshore_Wind_model\Country_lst.csv')
+    # country_lst = country_lst.Country.to_list()
+
+    # Now run the code around the world
+    for lat in range(0, 85):
+        for lon in range(0, 180):
+            try:
+                # Only use the countries you're interested in...
+                country = world[world.intersects(Point(lon, lat))].iloc[0].country
+            except IndexError:
+                country = 'None'
+            if country in country_lst:
+                # Get the weather data in the target location
+                location = plc.renewable_data(data, lat, lon, renewables)
+
+                # Run the code
+                result = main(n=n, weather_data=location.concat.drop(columns='Weights'),
+                              multi_site=True)
+                # Add the output to the data store.
+                store.add_location(lat, lon, country, result)
+        # # Output the data periodically just in case there's a power outage or similar...
+        # if lat % 20 == 0:
+        #     df = pd.DataFrame.from_dict(store.collated_results, orient='index')
+        #     df.to_csv('{a}_lat_{b}.csv'.format(a=year, b=lat))
+    # Output all the data at the end
+    df = pd.DataFrame.from_dict(store.collated_results, orient='index')
+    df.to_csv('{a}_Ukraine_NH3.csv'.format(a=year))
+
+def run_QLD_sites():
+    file_lst = glob.glob('Data/*.csv')
+    time_step = int(4 / 0.5)  # 0.5 included because 1/2 hour intervals in QLD
+    length = len(pd.read_csv(file_lst[0]))/time_step
+    network_design = generate_network(length, 'Basic_ammonia_plant_2030', aggregation_count=time_step)
+    store = pds.Data_store()
+    for file in file_lst:
+        result = main(n=network_design, file_name=file, multi_site=True, extension='_2030', aggregation_count=time_step)
+        store.add_location(file[5:-4], result)
+    df = pd.DataFrame.from_dict(store.collated_results, orient='index')
+    df.to_csv('2050_NH3_Costs_2.csv')
+
+
+if __name__ == '__main__':
+    # for year in [2030, 2035, 2040, 2045, 2050]:
+    #     run_Alli_sites(year)
+    run_QLD_sites()
+
---
+++ p_location_class.py
@@ -0,0 +1,155 @@
+"""File to reduce long periods of renewable data down to its midoids, and then design an ammonia plant off it"""
+import pandas as pd
+import numpy as np
+import glob
+import xarray as xr
+import os
+
+
+class all_locations:
+    # List of files and relevant information    
+    def __init__(self, path):
+        if path is None:
+            self.path = os.getcwd()
+        else:
+            self.path = path
+        self.file_list = glob.glob(self.path + r'\PowerData\*.nc')
+
+        self.Solars = []
+        self.Winds = []
+        self.SolarTrackings = []
+        for file in self.file_list:
+            if 'SolarTracking' in file:
+                self.SolarTrackings.append(xr.open_dataset(file))
+            elif 'Solar' in file:
+                self.Solars.append(xr.open_dataset(file))
+            elif 'WindPower' in file:
+                self.Winds.append(xr.open_dataset(file))
+        self.bathymetry = xr.open_dataset(self.path + r'\PowerData\model_bathymetry.nc')
+        self.waccs = pd.read_csv(self.path + r'\Equipment Data\WACCs.csv')
+
+    def in_ocean(self, latitude, longitude):
+        # Returns true if a location is in the ocean
+        # Returns false otherwise
+        return self.bathymetry.loc[dict(latitude=latitude, longitude=longitude)].depths.values.tolist()
+
+
+class renewable_data:
+    # Data stored for a specific renewable location, including cluster information
+
+    def __init__(self, data, latitude, longitude, renewables, aggregation_variable=1, aggregation_mode=None,
+                 wake_losses=0.93):
+        """Initialises the data class by importing the relevant file, loading the data, and finding the location.
+        Reshapes the data.
+        Note that df refers to just the data for the specific location as an xarray; not the data for all locations."""
+        self.longitude = longitude
+        self.latitude = latitude
+        self.wake_losses = 0.93
+        self.get_data_from_nc(data)
+        self.path = data.path
+        print('The plant is at latitude {latitude} and longitude {longitude}\n'.format(
+            latitude=self.latitude, longitude=self.longitude))
+
+        # Extract the relevant profile
+        self.renewables = renewables
+        self.get_data_as_list()
+        if aggregation_mode == 'optimal_cluster':
+            self.consecutive_temporal_cluster(aggregation_variable)
+        else:
+            self.aggregate(aggregation_variable)
+        # self.to_csv()
+
+    def to_csv(self):
+        """Sends output weather data to a csv file"""
+        output_file_name = '{a}_{b}_renewable_energy data.csv'.format(a=self.latitude, b=self.longitude)
+        self.concat.to_csv(output_file_name)
+
+    def get_data_from_nc(self, weather_data):
+        """Imports the data from the nc files and interprets them into wind and solar profiles"""
+        self.data = {}
+        if self.longitude < -60:
+            ref = 0
+        elif self.longitude < 60:
+            ref = 1
+        else:
+            ref = 2
+
+        self.data['Solar'] = weather_data.Solars[ref].loc[
+            dict(latitude=self.latitude, longitude=self.longitude)].Solar.values
+        self.data['Wind'] = weather_data.Winds[ref].loc[
+                                dict(latitude=self.latitude, longitude=self.longitude)].Wind.values * self.wake_losses
+        self.data['SolarTracking'] = weather_data.SolarTrackings[ref].loc[
+            dict(latitude=self.latitude, longitude=self.longitude)].Solar.values
+        self.hourly_data = pd.to_datetime(weather_data.Solars[ref].time.values)
+
+    def get_data_as_list(self):
+        """Extracts the data required and stores it in lists by hour"""
+        df = pd.DataFrame()
+        for source in self.renewables:
+            try:
+                edited_output = np.array(self.data[source])
+                df[source] = edited_output
+            except KeyError:
+                df[source] = np.ones(len(self.data[self.renewables[0]]))
+
+        self.concat = df
+
+    def aggregate(self, aggregation_count):
+        """Aggregates self.concat into blocks of fixed numbers of size aggregation_count. aggregation_count must be an integer which is a factor of 24 (i.e. 1, 2, 3, 4, 6, 12, 24)"""
+        """To be corrected to work without days/clusters"""
+        if self.concat.shape[0] % aggregation_count != 0:
+            raise TypeError("Aggregation counter must divide evenly into the total number of data points")
+
+        self.concat['Weights'] = np.ones(self.concat.shape[0]).tolist()
+        for i in range(0, self.concat.shape[0] // aggregation_count):
+            keep_index = i * aggregation_count
+            for j in range(1, aggregation_count):
+                drop_index = keep_index + j
+                self.concat.loc[keep_index] += self.concat.loc[drop_index]
+                self.concat.drop(drop_index, inplace=True)
+
+    def consecutive_temporal_cluster(self, data_reduction_factor):
+        """Reduces the data size by clustering adjacent hours until it has reduced in size by data_reduction_factor"""
+
+        if data_reduction_factor < 1:
+            raise TypeError("Data reduction factor must be greater than 1")
+
+        self.concat['Weights'] = np.ones(self.concat.shape[0]).tolist()
+        columns_to_sum = ['Solar', 'Wind']
+
+        proximity = []
+        for row in range(self.concat.shape[0]):
+            if row < self.concat.shape[0] - 1:
+                differences = sum(
+                    abs(self.concat[element].iloc[row] - self.concat[element].iloc[row + 1]) for element in
+                    columns_to_sum)
+                proximity.append(
+                    2 * differences * self.concat['Weights'].iloc[row] * self.concat['Weights'].iloc[row + 1] \
+                    / (self.concat['Weights'].iloc[row] + self.concat['Weights'].iloc[row + 1]))
+        proximity.append(1E6)
+        self.concat['Proximity'] = proximity
+
+        target_size = self.concat.shape[0] // data_reduction_factor
+        while self.concat.shape[0] > target_size:
+            keep_index = self.concat['Proximity'].idxmin()
+            i_keep_index = self.concat.index.get_indexer([keep_index])[0]
+            drop_index = self.concat.index.values[i_keep_index + 1]
+            self.concat.loc[keep_index] += self.concat.loc[drop_index]
+            self.concat.drop(drop_index, inplace=True)
+            if i_keep_index + 1 < len(self.concat):
+                differences = sum(
+                    abs(self.concat[element].iloc[i_keep_index] / self.concat['Weights'].iloc[i_keep_index] \
+                        - self.concat[element].iloc[i_keep_index + 1] / self.concat['Weights'].iloc[i_keep_index + 1]) \
+                    for element in columns_to_sum)
+                self.concat['Proximity'].iloc[i_keep_index] = 2 * differences * self.concat['Weights'].iloc[
+                    i_keep_index] \
+                                                              * self.concat['Weights'].iloc[i_keep_index + 1] \
+                                                              / (self.concat['Weights'].iloc[i_keep_index] +
+                                                                 self.concat['Weights'].iloc[i_keep_index + 1])
+        self.concat.drop(columns=['Proximity'], inplace=True)
+
+
+if __name__ == "__main__":
+    data = all_locations(None)
+    location = renewable_data(data, 3, 36, ['Wind', 'Solar', 'SolarTracking'])
+    location.to_csv()
